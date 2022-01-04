74f3b7ed014ae5790e2462ad6e59e9f9c1c1bc17
hotta <hotta-nagoya-6163@au.com>
2022-01-04 11:51:45 +0000

    Path brunch model test

--- app/models/brunch.rb
+++ app/models/brunch.rb
@@ -1,4 +1,9 @@
 class Brunch < ApplicationRecord
   # モデルの関係性
   belongs_to :company
+  
+  # 制約
+  validates :name,    presence: { message: '支社名を入力してください。' },
+                      length: { maximum: 137, message: '支社名が長すぎます。' },
+                      uniqueness: { scope: :company_id, message: 'その支店名は既に登録されています。' }
 end
