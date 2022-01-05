c159409364912c0a979717dbfc1fc49ff1707b28
hotta <hotta-nagoya-6163@au.com>
2022-01-05 06:58:43 +0000

    Render new when invalid signup params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,23 @@ class UsersController < ApplicationController
   end
   
   def create
+    @user = User.new(user_params)
+    if @user.save
+      
+    else
+      render 'new'
+    end
   end
 
   def show
   end
+  
+  private
+    def user_params
+      params.require(:user).permit( :first_name, 
+                                    :last_name,
+                                    :email,
+                                    :password,
+                                    :password_confirmation)
+    end
 end
