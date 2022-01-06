ad1a1d49c97f98add7db4e9ddac3f096c8b99beb
hotta <hotta-nagoya-6163@au.com>
2022-01-06 11:03:00 +0000

    Destroy relationship model

--- app/models/relationship.rb
+++
@@ -1,9 +0,0 @@
-class Relationship < ApplicationRecord
-  # モデルの関係性
-  belongs_to :user
-  belongs_to :branch
-  
-  # バリデーション
-  validates :user_id, presence: true
-  validates :branch_id, presence: true
-end
