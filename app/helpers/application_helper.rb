fee8ec0467571366c480568f0f53a7b6ec64f8c9
hotta <hotta-nagoya-6163@au.com>
2022-01-26 22:46:21 +0000

    New company_registration_controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,20 @@ module ApplicationHelper
     resource.save
     resource
   end
+  
+  def day_pull_down
+    [["日曜日", 0], ["月曜日", 1], ["火曜日", 2], ["水曜日", 3], ["木曜日", 4], ["金曜日", 5], ["土曜日", 6]]
+  end
+  
+  def date_pull_down
+    (1..30).inject([]) do |result, n|
+      if n == 1
+        result << ["1（初日）", n]
+      elsif n == 30
+        result << ["30（末日）", n]
+      else
+        result << [n, n]
+      end
+    end
+  end
 end
--- test/controllers/company_registrations_controller_test.rb
+++ test/controllers/company_registrations_controller_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class CompanyRegistrationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @company_name = "test_company"
+    @branch_name = "test_branch"
+  end
+  
+  test "should get new" do 
+    get new_company_registration_path
+    assert_response :success
+    # 共通フォーム
+    assert_select 'form[action=?]', company_registrations_path
+    assert_select 'input[name=?]', 'company[name]'
+    assert_select 'input[name=?]', 'branch[name]'
+    assert_select 'input[type=time][name=?]', 'branch[start_of_business_hours]'
+    assert_select 'input[type=time][name=?]', 'branch[end_of_business_hours]'
+  end
 end
