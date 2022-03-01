b3b68ad1a88ebed0d758d6336f50575011942e38
hotta <hotta-nagoya-6163@au.com>
2022-03-01 02:41:13 +0000

    ShiftSubmissionsHelperのform_numを削除

--- app/helpers/shift_submissions_helper.rb
+++ app/helpers/shift_submissions_helper.rb
@@ -1,16 +1,2 @@
 module ShiftSubmissionsHelper
-  def forms_num
-    case @branch.subtype
-    when Weekly
-      case @branch.subtype.num_of_weeks
-      when 1 then { default: 4, max: 7 }
-      when 2 then { default: 7, max: 14 }
-      end
-    when Monthly
-      case @branch.subtype.period_num
-      when 1 then { default: 15, max: 31 }
-      when 2 then { default: 8, max: 16 }
-      end
-    end
-  end
 end
