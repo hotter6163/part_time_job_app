9eac3fad66f46a807696087e4cd0444583449ebe
hotta <hotta-nagoya-6163@au.com>
2022-03-06 03:23:50 +0000

    オウム返しが出来るようにした

--- app/controllers/line_bots_controller.rb
+++ app/controllers/line_bots_controller.rb
@@ -5,14 +5,30 @@ class LineBotsController < ApplicationController
   before_action :validate_signature
   
   def callback
+    events = client.parse_events_from(@body)
+
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    end
+    
     "OK"
   end
   
   private
     def validate_signature
-      body = request.body.read
+      @body = request.body.read
       signature = request.env['HTTP_X_LINE_SIGNATURE']
-      unless client.validate_signature(body, signature)
+      unless client.validate_signature(@body, signature)
         halt :bad_request
       end
     end
