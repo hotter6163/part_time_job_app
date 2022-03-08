4d6b39edcedb881105128fb2837d6014c4f11d2a
hotta <hotta-nagoya-6163@au.com>
2022-03-08 13:46:14 +0000

    ユーザー連携のURLを発行するところまで

--- app/controllers/line_bots_controller.rb
+++ app/controllers/line_bots_controller.rb
@@ -1,12 +1,13 @@
 class LineBotsController < ApplicationController
   include Line::Client
+  include JSON
   
   skip_before_action :verify_authenticity_token
   before_action :validate_signature
   
   def callback
     events = client.parse_events_from(@body)
-
+    
     events.each do |event|
       case event
       when Line::Bot::Event::Message
@@ -17,6 +18,41 @@ class LineBotsController < ApplicationController
             text: event.message['text']
           }
           client.reply_message(event['replyToken'], message)
+        end
+      when Line::Bot::Event::Postback
+        data = parse(event["postback"]["data"])
+        case data[:action]
+        when :link
+          case data[:user]
+          when :exist
+            res = client.create_link_token(event["source"]["userId"])
+            link_token = JSON.parse(res.body)["linkToken"]
+            messages = [
+              {
+                type: 'text',
+                text: '以下のリンクからアプリにログインしてください。'
+              },
+              {
+                type: 'text',
+                text: new_user_session_url(linkToken: link_token)
+              }
+            ]
+            client.reply_message(event["replyToken"], messages)
+          when :new
+            res = client.create_link_token(event["source"]["userId"])
+            link_token = JSON.parse(res.body)["linkToken"]
+            messages = [
+              {
+                type: 'text',
+                text: '以下のリンクからユーザー登録を行ってください。'
+              },
+              {
+                type: 'text',
+                text: new_user_registration_url(linkToken: link_token)
+              }
+            ]
+            client.reply_message(event["replyToken"], messages)
+          end
         end
       end
     end
@@ -31,5 +67,14 @@ class LineBotsController < ApplicationController
       unless client.validate_signature(@body, signature)
         halt :bad_request
       end
+    end
+    
+    def parse(str)
+      result = {}
+      str.split("&").each do |item|
+        key, value = item.split("=")
+        result[key.to_sym] = value.to_sym
+      end
+      result
     end
 end
